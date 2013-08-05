c233780863a429675f408bd9bacae28e212770f5
Artur Moroz <morozarturv@gmail.com>
2013-08-05 12:22:04 +0300

    fixes

--- js/common.js
+++ js/common.js
@@ -3,39 +3,38 @@ $(document).ready(function() {
 //choice city
 function select() {
 	var el = $('.js-select');
-	var city = el.find('span');
+	var el_title = el.find('span');
 	var item = el.find('li');
 	var list = el.find('ul');
-	city.click(function() {
-		el.removeClass('is-open');
-		list.hide();
-		$(this).parent().toggleClass('is-open');
-		$(this).next().toggle();
+	el_title.click(function() {	
+		if ($(this).parent().hasClass('is-open')) {
+			$(this).parent().removeClass('is-open');
+		}
+		else {
+			el.removeClass('is-open');
+			$(this).parent().addClass('is-open');
+		};
 	});
 	item.click(function() {
 		var val = $(this).text();
 		$(this).parent().prev().addClass('is-selected');
 		$(this).parent().prev().html(val + '<i></i>');
-		$(this).parent().hide();
 		el.removeClass('is-open');
 	});
-	el.click(function(event){
+	el_title.click(function(event){
 	  event.stopPropagation();
 	});
 };
+select();
 
 //faq accos
 $('.js-accos').click(function() {
 	$(this).next().slideToggle();
 })
 
-//init
-select();
-
 //click document
 $(document).click(function() {
-	$('.js-select').removeClass('is-open');
-	$('.js-select ul').hide();
+	$('.js-select').removeClass('is-open');	
 });
 
 
